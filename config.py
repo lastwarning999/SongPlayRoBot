761607c6900d0510f18848384d49f23021f21dbe
lastwarning999 <167008968+lastwarning999@users.noreply.github.com>
2024-05-13 17:17:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ import os
 API_ID = int(os.getenv("20401009"))
 API_HASH = os.getenv("6445b43e4af228532e84848d1bb2eb64")
 BOT_TOKEN = os.getenv("6882280996:AAFHCrV0s8yep4LGtYBXxoQQWMDS4oVy0i0")
-DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = os.getenv("https://songplayrobot-n2c9.onrender.com")
 OWNER_ID = list({int(x) for x in os.environ.get("6975923843", "").split()})
